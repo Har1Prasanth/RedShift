3f601d7290593bf7101012cfa1cb060c4b4b023e
Har1Prasanth <hariprasanthirah@gmail.com>
2022-03-11 15:37:15 +0530

    Helloworld
    Demo

---
+++ src/main/java/com/accenture/lkm/controller/HelloDemo.java
@@ -0,0 +1,20 @@
+package com.accenture.lkm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloDemo {
+
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String helloWorld() throws Exception {
+		{
+			String str1="Hello, My Name is Hari Prasanth!";
+			return str1;
+		}
+	}
+
+	
+}
